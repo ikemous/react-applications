41491da755878869f4323000658fcee35f7ba604
ike barranco <barranco.ike@outlook.com>
2020-09-18 16:16:48 -0600

    redux alterations to be able to change the payload

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/actions";
 
 function Counter() {
     const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
 
     return (
-        <>
-        </>
+        <div className="App">
+            <h1>Counter App</h1>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <p>{counter}</p>
+            <button onClick={() => dispatch(decrement())}>-</button>
+        </div>
     );
 };
 
