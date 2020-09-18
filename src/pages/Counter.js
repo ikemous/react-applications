ff2dd20a1b14f32941faa00e1191a505da667f8f
ike barranco <barranco.ike@outlook.com>
2020-09-18 17:47:53 -0600

    added buttons for the ability to change count easily

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Form, Col, Button } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, updateCount } from "../redux/actions";
 
@@ -8,20 +9,39 @@ function Counter() {
     const dispatch = useDispatch();
 
     const handleChange = (event) => {
-        console.log(event.target.value <= Math.abs(100));
-        if (!isNaN(event.target.value) && event.target.value <= 100 && event.target.value >= 100) {
-            return dispatch(updateCount(event.target.value));
+        if (!isNaN(event.target.value) && event.target.value <= 100 && event.target.value >= 0) {
+            return dispatch(updateCount(parseInt(event.target.value)));
         }
         console.log("%c Invalid Number:", "color:red;");
         console.log("%c Number Must Be Between 0 and 100", "color:orange;");
     }
     return (
         <div className="App">
-            <input type="text" onChange={handleChange} />
             <h1>Counter App</h1>
             <button onClick={() => dispatch(increment(count))}>+</button>
             <p>{counter}</p>
             <button onClick={() => dispatch(decrement(count))}>-</button>
+            <Form>
+                <Form.Row>
+                    <Col>
+                        <Form.Control placeholder="First name" onChange={handleChange} />
+                    </Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col>
+                        <Button onClick={() => dispatch(updateCount(1))}>1</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => dispatch(updateCount(5))}>5</Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => dispatch(updateCount(10))}>10</Button>
+                    </Col>
+                    <Col>
+                        <Button  onClick={() => dispatch(updateCount(100))}>100</Button>
+                    </Col>
+                </Form.Row>
+            </Form>
         </div>
     );
 };
