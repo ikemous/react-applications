1aaeff79e7306087660808c6511bd985b7b34511
ike barranco <barranco.ike@outlook.com>
2020-09-19 01:11:57 -0600

    finished styling of counter app

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./counter.css";
 import { Form, Col, Row, Button, Container } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,25 +7,29 @@ import { increment, decrement, updateCount } from "../redux/actions";
 function Counter() {
     const counter = useSelector(state => state.counter);
     const count = useSelector(state => state.count);
+    const [error, setError] = useState(false);
     const dispatch = useDispatch();
 
     const handleChange = (event) => {
         if (!isNaN(event.target.value) && event.target.value <= 100 && event.target.value >= 0) {
+            setError(false);
             return dispatch(updateCount(parseInt(event.target.value)));
         }
+        setError(true);
         console.log("%c Invalid Number:", "color:red;");
         console.log("%c Number Must Be Between 0 and 100", "color:orange;");
     }
+
     return (
-        <Container style={{minHeight: "100vh", textAlign: "center"}}>
+        <Container style={{minHeight: "100vh", textAlign: "center", background: "skyblue"}} fluid>
         <h1 style={{margin: "0", padding: "20px"}}>Counter App</h1>
             <div className="d-flex justify-content-center align-items-center" style={{height:"calc(100vh - 88px)"}}>
                 <div style={{width: "100%"}}>
-                    <Row style={{paddingBottom: "30vh"}}>
+                    <Row style={{paddingBottom: "20vh"}}>
                         <Col>
-                            <button style={{padding:"15px", fontSize: "2rem"}} className="inline-item" onClick={() => dispatch(increment(count))}>+</button>
-                            <input className="inline-item counter" style={counter === 0? {color: "black"}: counter > 0? {color: "green"}: {color: "red"}} value ={counter} disabled/>
-                            <button style={{padding:"15px", fontSize: "2rem"}} className="inline-item" onClick={() => dispatch(decrement(count))}>-</button>
+                            <Button variant="success" style={{padding:"15px", fontSize: "2rem"}} className="inline-item" onClick={() => dispatch(increment(count))}>+</Button>
+                            <input aria-label="counter" className="inline-item counter" style={counter === 0? {color: "black"}: counter > 0? {color: "green"}: {color: "red"}} value ={counter} disabled/>
+                            <Button variant="danger" style={{padding:"15px", fontSize: "2rem"}} className="inline-item" onClick={() => dispatch(decrement(count))}>-</Button>
                         </Col>
                     </Row>
                     <Form onSubmit={(event) => event.preventDefault()}>
@@ -33,9 +37,14 @@ function Counter() {
                             <Col 
                                 sm={12}
                                 md={{ span: 6, offset: 3 }}
-                            >
-                                <Form.Label>Enter Custom Count</Form.Label>
-                                <Form.Control placeholder="Custom Count" onChange={handleChange} />
+                            >  
+                                <Form.Group>
+                                    <Form.Label>Enter Custom Count Or Use Quick Change Buttons</Form.Label>
+                                    <Form.Control isInvalid={error} placeholder="Custom Count" onChange={handleChange} />
+                                    <Form.Control.Feedback type="invalid">
+                                        Number Must Be Between 0 And 100
+                                    </Form.Control.Feedback>
+                                </Form.Group>
                             </Col>
                         </Form.Row>
                         <Form.Row>
