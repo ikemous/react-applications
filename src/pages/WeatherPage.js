9e6cbf463716d8d715fb25c6fb65de6794f868b3
Ike <barranco.ike@gmail.com>
2020-09-21 03:14:46 -0600

    added items to be able to pull info off of store

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -1,26 +1,32 @@
 import React, { useEffect } from "react";
 import { Container, Image, Form, Row, Button } from "react-bootstrap";
+import { updateCity, updateWeatherResults } from "../redux/actions"
+import { useSelector, useDispatch } from "react-redux";
 import "./weatherPage.css";
 
 function WeatherPage() {
-
+    const city = useSelector(state => state.city);
+    const weatherResults = useSelector(state => state.weatherResults);
+    const dispatch = useDispatch();
     useEffect(() => {
 
     }, []);
 
     return( 
         <Container>
-            <h1>Weather APP</h1>
+            <h1 className="text-center">Weather APP</h1>
             <Row className="d-flex justify-content-center align-items-center text-center">
-                <Image style={{display:"block"}} alt="cloud to indicate weather" src="https://via.placeholder.com/150" roundedCircle  />
-                <h2>Using Weather API</h2>
-                <Form>
-                    <Form.Group>
-                        <Form.Label htmlFor="cityInput">Search Your City</Form.Label>
-                        <Form.Control id="cityInput" type="text" />
-                        <Button>Search</Button>
-                    </Form.Group>
-                </Form>
+                <div>
+                    <Image style={{display:"block", margin:"auto"}} alt="cloud to indicate weather" src="https://via.placeholder.com/150" roundedCircle  />
+                    <h2>Using Weather API</h2>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label htmlFor="cityInput">Search Your City</Form.Label>
+                            <Form.Control id="cityInput" type="text" />
+                            <Button>Search</Button>
+                        </Form.Group>
+                    </Form>
+                </div>
             </Row>
         </Container>
     );
