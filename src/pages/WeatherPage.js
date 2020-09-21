f15a72c9707b926170dc1ae30e8d6e091cef14a9
Ike <barranco.ike@gmail.com>
2020-09-21 02:49:14 -0600

    added weather page and a file that is excluded to hide API keys

---
+++ src/pages/WeatherPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import "./weatherPage.css";
+
+function WeatherPage() {
+
+    useEffect(() => {
+
+    }, []);
+
+    return( 
+        <>
+        </>
+    );
+};
+
+export default WeatherPage;
