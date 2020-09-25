f85adebcb6cabe3b99759e86e32416bcda3ca831
Ike <barranco.ike@gmail.com>
2020-09-25 06:44:04 -0600

    added react helmet to change metadata and title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import weatherImage from "../utils/images/weather.jpg";
 import counterImage from "../utils/images/counter.jpg";
 import timerImage from "../utils/images/timer.jpg";
-import { Link } from "react-router-dom"
 import ShowMeTheDate from "../components/ShowMeTheDate";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom"
 import { RootStateOrAny, connect } from "react-redux";
 import { updateDate } from "../utils/actions";
 import { Carousel, Container } from "react-bootstrap";
@@ -36,44 +37,53 @@ class HomePage extends React.Component<Props> {
 
     render()  {
         return(
-            <Container style={{background: "#376dae", height: "calc(100vh - 56px)"}} fluid>
-                <ShowMeTheDate date={this.props.global.date} />
-                <Carousel style={{height: "70vh"}}>
-                    <Carousel.Item as={Link} to="/weather">
-                        <img
-                            className="d-block w-100 slideImage"
-                            src={weatherImage}
-                            alt="Weather Application Preview"
-                        />
-                        <Carousel.Caption className="slideText">
-                        <h3>Weather Application</h3>
-                        <p>Application To Search And View Current Weather</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item as={Link} to="/counter">
-                        <img
-                            className="d-block w-100 slideImage"
-                            src={counterImage}
-                            alt="Counter Application Preview"
-                        />
-                        <Carousel.Caption className="slideText">
-                        <h3>Counter Application</h3>
-                        <p>Application Count Up Or Down On Click</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item as={Link} to="/timer">
-                        <img
-                            className="d-block w-100 slideImage"
-                            src={timerImage}
-                            alt="Timer Application Preview"
-                        />
-                        <Carousel.Caption className="slideText">
-                        <h3>Timer Application</h3>
-                        <p>Application To Count Down On Starting Point</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </Container>
+            <>
+                <Helmet>
+                    <title>Reactications - Home</title>    
+                    <meta
+                        name="description"
+                        content="Page to show the previews of the projects inside this website"
+                    />
+                </Helmet>
+                <Container style={{background: "#376dae", height: "calc(100vh - 56px)"}} fluid>
+                    <ShowMeTheDate date={this.props.global.date} />
+                    <Carousel style={{height: "70vh"}}>
+                        <Carousel.Item as={Link} to="/weather">
+                            <img
+                                className="d-block w-100 slideImage"
+                                src={weatherImage}
+                                alt="Weather Application Preview"
+                            />
+                            <Carousel.Caption className="slideText">
+                            <h3>Weather Application</h3>
+                            <p>Application To Search And View Current Weather</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item as={Link} to="/counter">
+                            <img
+                                className="d-block w-100 slideImage"
+                                src={counterImage}
+                                alt="Counter Application Preview"
+                            />
+                            <Carousel.Caption className="slideText">
+                            <h3>Counter Application</h3>
+                            <p>Application Count Up Or Down On Click</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item as={Link} to="/timer">
+                            <img
+                                className="d-block w-100 slideImage"
+                                src={timerImage}
+                                alt="Timer Application Preview"
+                            />
+                            <Carousel.Caption className="slideText">
+                            <h3>Timer Application</h3>
+                            <p>Application To Count Down On Starting Point</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Container>
+            </>
         );
     }
 };
