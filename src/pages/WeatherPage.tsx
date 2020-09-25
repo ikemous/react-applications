468e454941dce81ee3a041d02cf52c6a2b91fc2d
Ike <barranco.ike@gmail.com>
2020-09-25 06:48:44 -0600

    added react helmet to page for title and meta description

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import API from "../utils/API";
 import FindMyCityForm from "../components/FindMyCityForm";
+import { Helmet } from "react-helmet";
 import PrettyDecentWeatherCard from "../components/PrettyDecentWeatherCard";
 import { Container, Row } from "react-bootstrap";
 import { useSelector, useDispatch, RootStateOrAny} from "react-redux";
@@ -19,19 +20,28 @@ function WeatherPage() {
     };
 
     return (
-        <Container style={{background: "#376dae", minHeight: "calc(100vh - 56px)"}} fluid>
-            <Row 
-                className="d-flex justify-content-center align-items-center text-center"
-                style={{minHeight: "calc(100vh - 56px)"}}
-            >
-                <div style={{width: "100%"}}>                
-                    <h1>Weather APP</h1>
-                    <h2>Using Weather API</h2>
-                    <FindMyCityForm searchCity={searchCity} />
-                    <PrettyDecentWeatherCard />
-                </div>
-            </Row>
-        </Container>
+        <>
+            <Helmet>
+                <title>Reactications - Weather</title>
+                <meta
+                    name="description"
+                    content="Page to Allow user to be able to search for a city and show the current weather"
+                />
+            </Helmet>
+            <Container style={{background: "#376dae", minHeight: "calc(100vh - 56px)"}} fluid>
+                <Row 
+                    className="d-flex justify-content-center align-items-center text-center"
+                    style={{minHeight: "calc(100vh - 56px)"}}
+                >
+                    <div style={{width: "100%"}}>                
+                        <h1>Weather APP</h1>
+                        <h2>Using Weather API</h2>
+                        <FindMyCityForm searchCity={searchCity} />
+                        <PrettyDecentWeatherCard />
+                    </div>
+                </Row>
+            </Container>
+        </>
     );
 };
 
