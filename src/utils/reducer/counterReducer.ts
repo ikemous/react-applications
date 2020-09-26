0aa4242d55ad747b01824b76eedd73698740a064
ike barranco <barranco.ike@outlook.com>
2020-09-26 05:03:10 -0600

    added comments for readability

--- src/utils/reducer/counterReducer.ts
+++ src/utils/reducer/counterReducer.ts
@@ -1,79 +1,53 @@
-// Type for Action variables in reducer 
-// functions
+/**
+ * Action type
+ * Purpose: To State What Items are in the action
+ * type: string to tell what action is to be taken
+ * payload: number or boolean to update the state with 
+ */
 type Action = {
     type: string,
-    payload: number
-}
+    payload: any
+} //End type Action
 
+/**
+ * CounterState interface
+ * Purpose: Description of State for the CounterState
+ * count: number to display to use on count amount
+ * counter: number to alter count with
+ * error: boolean to validate user input
+ */
 interface CounterState {
     count: number,
     counter: number,
     error: boolean,
-};
+}; // End interface CounterState
 
-// Type for counter variables
-type Counter = number;
-
-// Starting values
-const startingCount = 0;
-const startingCounter = 1;
-
+/**
+ * counterPageReducers()
+ * Purpose: To Alert Count, counter or error on action type
+ * Parameters:
+ *      state - see interface CounterState
+ *      action - see type Action
+ * return: 
+ *      state - Updated count/counter/error or current state
+ */
 export const counterPageReducer = (state: CounterState = {count: 0, counter: 1, error: false}, action: Action) => {
+    // Check Action type
     switch(action.type) {
+        // Increment Count By Payload
         case "INCREMENT":
             return {...state, count: (state.count + action.payload)};
+        // Decrement Count By Payload
         case "DECREMENT":
             return {...state, count: (state.count - action.payload)};
+        // Update Counter By Payload
         case "UPDATE":
-            return {...state, counter: action.payload}
+            return {...state, counter: action.payload};
+        // Update Error By Payload
         case "UPDATE_COUNTER_ERROR":
             return {...state, error: action.payload};
+        // No Action return State
         default:
             return state;
     };
-};
-
-/**
- * countReducer()
- * Purpose: Increment or decrement 
- * Parameters:
- *      state - current count state, default is 0
- *      action - object containing a type and payload
- * return: 
- *      state - count after action or current state
- */
-export const countReducer = (state: Counter = startingCount, action: Action) => {
-    // Check action Type
-    switch (action.type) {
-        // Increment count state
-        case "INCREMENT":
-            return state + action.payload
-        // decrement count state
-        case "DECREMENT":
-            return state - action.payload
-        // no action, return current state
-        default:
-            return state;
-    }
-}; // End countReducer()
-
-/**
- * counterReducer()
- * Purpose: Update counter for count increment/decrement
- * Parameters:
- *      state - counter for increment/decrement, default is 1
- *      action - object containing a type and payload
- * return: 
- *      state - updated counter or return current state 
- */
-export const counterReducer = (state: Counter = startingCounter, action: Action) => {
-    // Check Action
-    switch (action.type) {
-        // Update counter state
-        case "UPDATE":
-            return state = action.payload;
-            // No action, return state
-        default:
-            return state;
-    }
-}; // End counterReducer()
+}; // End counterPageReducer()
--- src/utils/reducer/index.ts
+++ src/utils/reducer/index.ts
@@ -2,13 +2,11 @@ import { passwordReducer } from "./passwordReducer";
 import { homeReducer } from "./pageReducer";
 import { weatherReducer } from "./weatherReducer";
 import { timerReducer } from "./timerReducer";
-import { counterPageReducer, counterReducer, countReducer } from "./counterReducer";
+import { counterPageReducer } from "./counterReducer";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
     password: passwordReducer,
-    count: countReducer,
-    counter: counterReducer,
     global: homeReducer,
     weatherState: weatherReducer,
     timerState: timerReducer,
